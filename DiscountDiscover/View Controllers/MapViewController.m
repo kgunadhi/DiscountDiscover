8aeeb4b5470d0f2954cf26c353b9a5d838946e1e
Kaitlyn Gunadhi <kgunadhi@fb.com>
2020-07-30 08:59:49 -0700

    Tapping marker shows deal details view

--- DiscountDiscover/View Controllers/MapViewController.m
+++ DiscountDiscover/View Controllers/MapViewController.m
@@ -10,8 +10,9 @@
 #import "APIManager.h"
 #import <CoreLocation/CoreLocation.h>
 #import "LocationManager.h"
+#import "DetailsViewController.h"
 
-@interface MapViewController () <CLLocationManagerDelegate>
+@interface MapViewController () <GMSMapViewDelegate>
 
 @property (nonatomic, strong) GMSMapView *mapView;
 
@@ -30,6 +31,8 @@
     self.mapView.myLocationEnabled = YES;
     [self.view addSubview:self.mapView];
     
+    self.mapView.delegate = self;
+    
     [self addDealMarkers];
 }
 
@@ -37,6 +40,24 @@
     for (Deal *deal in self.deals) {
         deal.marker.map = self.mapView;
     }
+}
+
+- (void)mapView:(GMSMapView *)mapView didTapInfoWindowOfMarker:(GMSMarker *)marker {
+    
+    NSUInteger i = [self.deals indexOfObjectPassingTest:^BOOL(Deal *deal, NSUInteger idx, BOOL *stop) {
+        return [deal.name isEqual:marker.snippet];
+    }];
+    Deal *deal = self.deals[i];
+    [self performSegueWithIdentifier:@"detailsSegue" sender:deal];
+}
+
+#pragma mark - Navigation
+
+- (void)prepareForSegue:(UIStoryboardSegue *)segue sender:(id)sender {
+    
+    // Details view segue
+    DetailsViewController *detailsViewController = [segue destinationViewController];
+    detailsViewController.deal = sender;
 }
 
 @end
